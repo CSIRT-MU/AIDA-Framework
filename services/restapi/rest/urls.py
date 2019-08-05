5a3dee4d4d42473b47535ea09404d73552a19481
Milan Ziaran <milan.ziaran@gmail.com>
2019-08-05 16:33:56 +0200

    add REST API

---
+++ services/restapi/rest/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'rest'
+urlpatterns = [
+    url(r'^componentsinfo/$', views.system_state_with_more_info, name='system_state_with_more_info'),
+    url(r'^db/rules/(?P<date1>[0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.get_rules, name='get_rules'),
+    url(r'^db/activerules/$', views.active_rules, name='active_rules'),
+    url(r'^db/activerules/(?P<rule_id>\d+)/$', views.activate_rule, name='activate_rule'),
+    url(r'^db/inactiverules/(?P<rule_id>\d+)/$', views.deactivate_rule, name='deactivate_rule'),
+    url(r'^db/deleterules/(?P<rule_id>\d+)/$', views.delete_rule, name='delete_rule'),
+    url(r'^db/addrules/(?P<given_rule>[a-zA-Z0-9._]{3,},[a-zA-Z0-9._]{3,} => [a-zA-Z0-9._]{3,})/$', views.add_rule, name='add_rule'),
+    url(r'^enforcedatamining/$', views.enforce_data_mining, name='enforce_data_mining'),
+    url(r'^reloadrulematching/$', views.reload_rule_matching, name='reload_rule_matching')
+]
---
+++ services/restapi/rest/views.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+from configparser import ConfigParser
+# from json import loads
+from os import popen
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from sysdmanager import SystemdManager
+from time import sleep
+
+import sqlite3
+
+CFG_FILE = '/var/www/restapi/rest/config.ini'
+
+
+def get_manager():
+    return SystemdManager()
+
+
+def connect_to_sqlite3_db():
+    """
+    Connects to the sqlite3 database.
+    :return: Connection of type sqlite3.Connection.
+    """
+    config = ConfigParser()
+    config.read(CFG_FILE)
+    conn = sqlite3.connect(config['paths']['path_to_database'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def execute_sql(sql, rule_id):
+    """
+    Executes sql command with given rule id.
+    :param sql: Sql command to be executed.
+    :param rule_id: Rule id.
+    """
+    conn = connect_to_sqlite3_db()
+    conn.row_factory = sqlite3.Row
+    conn.cursor().execute(sql, rule_id)
+    conn.commit()
+    conn.cursor().close()
+
+
+def is_running(service):
+    """
+    Checks if service is running using sysdmanager library.
+    :param service: Service to be checked.
+    :return: Information if service is running or not.
+    """
+    manager = get_manager()
+    if manager.is_active(service + ".service"):
+        return 1
+    return 0
+
+
+def print_info(service):
+    """
+    Prints info about service.
+    :param service: Service which info will be printed.
+    :return: System call containing info.
+    """
+    cmd = 'systemctl status ' + service + '.service -n 0'  # '-n 0' disables printing logs
+    return popen(cmd).read()
+
+
+def print_logs(service):
+    """
+    Prints last n logs of a service, n set to 5.
+    :param service: Service which logs will be printed.
+    :return: System call containing info.
+    """
+    cmd = 'journalctl -u ' + service + '.service -n 5'
+    return popen(cmd).read()
+
+
+@api_view(['GET'])
+def system_state_with_more_info(request):
+    components_dict = {'kafka': {'status': is_running('kafka'),
+                                 'description': print_info('kafka'),
+                                 'log': print_logs('kafka')},
+                       'kafka_filer': {'status': is_running('kafka-filer'),
+                                       'description': print_info('kafka-filer'),
+                                       'log': print_logs('kafka-filer')},
+                       'warden': {'receiver': {'status': is_running('warden_filer_receiver'),
+                                               'description': print_info('warden_filer_receiver'),
+                                               'log': print_logs('warden_filer_receiver')},
+                                  'sender': {'status': is_running('warden_filer_sender'),
+                                             'description': print_info('warden_filer_sender'),
+                                             'log': print_logs('warden_filer_sender')}
+                                  },
+                       # TODO remove relationship_discovery if it won't be needed
+                       'spark': {'relationship_discovery': {'status': is_running('sparkapp-graphdb'),
+                                                            'description': print_info('sparkapp-graphdb'),
+                                                            'log': print_logs('sparkapp-graphdb')},
+                                 'sanitization': {'status': is_running('sanitization'),
+                                                  'description': print_info('sanitization'),
+                                                  'log': print_logs('sanitization')
+                                                  },
+                                 'aggregation': {'status': is_running('sparkapp-aggregation'),
+                                                 'description': print_info('sparkapp-aggregation'),
+                                                 'log': print_logs('sparkapp-aggregation')},
+                                 'data_mining': {'status': is_running('sparkapp-mining'),
+                                                 'description': print_info('sparkapp-mining'),
+                                                 'log': print_logs('sparkapp-mining')}
+                                 },
+                       # TODO esper and feedback components status
+                       'esper': {'status': 0,
+                                 'description': "",
+                                 'log': ""},
+                       'feedback': {'status': 0,
+                                    'description': "",
+                                    'log': ""}
+                       }
+
+    return Response(components_dict)
+
+
+# -------------------------------------------------DATABASE RELATED-----------------------------------------------------
+
+@api_view(['GET'])
+def get_rules(request, date1):
+    date1 = str(date1) + '%'
+    return Response(connect_to_sqlite3_db().cursor().execute("SELECT * FROM rule WHERE inserted LIKE ?", (date1,)))
+
+
+@api_view(['GET'])
+def active_rules(request):
+    sql = "SELECT * FROM rule WHERE active=1"
+    return Response(connect_to_sqlite3_db().cursor().execute(sql))
+
+
+@api_view(['POST'])
+def activate_rule(request, rule_id):
+    rule_id = (int(rule_id),)
+    sql = "UPDATE rule SET active=1 WHERE id=?"
+    execute_sql(sql, rule_id)
+    return Response()
+
+
+@api_view(['POST'])
+def deactivate_rule(request, rule_id):
+    rule_id = (int(rule_id),)
+    sql = "UPDATE rule SET active=0 WHERE id=?"
+    execute_sql(sql, rule_id)
+    return Response()
+
+
+@api_view(['POST'])  # TODO change http method to delete
+def delete_rule(request, rule_id):
+    rule_id = (int(rule_id),)
+    sql = "DELETE FROM rule WHERE id=?"
+    execute_sql(sql, rule_id)
+    return Response()
+
+
+@api_view(['POST'])
+def add_rule(request, given_rule):
+    count_id = 1
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    conn = connect_to_sqlite3_db()
+    for _ in conn.cursor().execute("SELECT * FROM rule"):
+        count_id += 1
+    sql = "INSERT INTO rule VALUES (?,?,?,0,0,0.0,0,NULL,'custom','')"
+    conn.cursor().execute(sql, (count_id, now, given_rule))
+    conn.commit()
+    conn.cursor().close()
+    return Response()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+@api_view(['POST'])
+def enforce_data_mining(request):
+    # TODO
+    return Response()
+
+
+@api_view(['POST'])
+def reload_rule_matching(request):
+    manager = get_manager()
+    if is_running("matching"):
+        manager.stop_unit("matching.service")
+        sleep(5)
+        manager.start_unit("matching.service")
+    else:
+        manager.start_unit("matching.service")
+    return Response()
