bc9ceb9cd3ddc8602a8b6557f9617f1d8bc897ee
Jaroslav Kaspar <kaspar.jarek@gmail.com>
2019-10-13 21:27:56 +0200

    Add CORS headers into Spark UI

---
+++ commons/spark-ui-filters/src/main/java/cz/muni/csirt/aida/spark/CorsFilter.java
@@ -0,0 +1,31 @@
+package cz.muni.csirt.aida.spark;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("X-Frame-Options", "allow-from http://localhost/");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
